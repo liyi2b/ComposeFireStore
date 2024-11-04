247d6e021fb3e11c125cafd0dc7ad1b7bf04fe2c
liyi2b <janis5201023@gmail.com>
2024-11-04 16:31:46 +0800

    v7_新增資料(自動產生id)

--- app/src/main/java/tw/edu/pu/csim/s1114702/composefirestore/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/s1114702/composefirestore/MainActivity.kt
@@ -27,6 +27,8 @@ import androidx.compose.material3.Button
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.ui.text.input.KeyboardType
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
 
 
 class MainActivity : ComponentActivity() {
@@ -55,7 +57,7 @@ fun Birth(m: Modifier){
     var userWeight by remember { mutableStateOf(3800)}
     var userPassword by remember { mutableStateOf("")}
     var msg by remember { mutableStateOf("訊息")}
-
+    val db = Firebase.firestore
 
 
     Column {
@@ -101,7 +103,16 @@ fun Birth(m: Modifier){
                 + "\n密碼：$userPassword")
 
         Row {
-            Button(onClick = {  }) {
+            Button(onClick = { val user = Person(userName, userWeight, userPassword)
+                db.collection("users")
+                    .add(user)
+                    .addOnSuccessListener { documentReference ->
+                        msg = "新增/異動資料成功"
+                    }
+                    .addOnFailureListener { e ->
+                        msg = "新增/異動資料失敗：" + e.toString()
+                    }
+            }) {
                 Text("新增/修改資料")
             }
             Button(onClick = {  }) {
@@ -113,6 +124,15 @@ fun Birth(m: Modifier){
         }
         Text(text = msg)
     }
+
 }
+
+
+data class Person(
+    var userName: String,
+    var userWeight: Int,
+    var userPassword: String
+)
+
 
 
